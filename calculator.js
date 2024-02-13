68498abff1d50c17d50fedd1b0c5e7f29401a522
akhremYuri <akhremr@gmail.com>
2024-02-13 23:04:05 +0300

    History of calculation is added

--- calculator.js
+++ calculator.js
@@ -1,54 +1,65 @@
-const display = document.querySelector('#display');
+const display = document.querySelector("#display");
+const calcLog = document.querySelector("#calcLog");
 
 let isCalculationPerformed = false;
 
-function appendDisplay(input){
-  if(isCalculationPerformed){
-    display.value = '';
+function appendDisplay(input) {
+  if (isCalculationPerformed) {
+    display.value = "";
     isCalculationPerformed = false;
   }
   display.value = display.value + input;
 }
 
-function clearDisplay(){
-  display.value = '';
+function clearDisplay() {
+  display.value = "";
 }
 
 function percentageDisplay() {
   display.value = display.value / 100;
-
 }
 
-function calculate(){
-
-
-  const expression = display.value.trim();
+function calculateExpr(expression) {
+  //const expression = display.value.trim();
   const operators = expression.match(/[\+\-\*\/]/g);
   const numbers = expression.split(/[\+\-\*\/]/g).map(parseFloat);
   let result = numbers[0];
 
-  for (let i = 0; i < operators.length; i++){
+  for (let i = 0; i < operators.length; i++) {
     const operator = operators[i];
     console.log(operator);
 
     const nextNumber = numbers[i + 1];
-    if (operator === '+'){
+    if (operator === "+") {
       result = result + nextNumber;
-    }
-    else if(operator === '-'){
+    } else if (operator === "-") {
       result = result - nextNumber;
-    }
-    else if(operator === '*'){
+    } else if (operator === "*") {
       result = result * nextNumber;
-    }
-    else if(operator === '/'){
+    } else if (operator === "/") {
       result = result / nextNumber;
     }
   }
-  display.value = result;
+  //display.value = result;
+  //isCalculationPerformed = true;
+  return result;
+}
+
+function addListItemToLogList(expression) {
+  const li = document.createElement("li");
+  li.className = "logItem";
+  li.innerHTML = expression;
+  calcLog.append(li);
+}
+
+function calculate(expression = display.value, addToLog = true) {
+  expression = expression.trim();
+  display.value = calculateExpr(expression);
+  if (addToLog) addListItemToLogList(expression);
   isCalculationPerformed = true;
+}
 
-
-
-
+calcLog.onclick = function (event) {
+  let target = event.target;
+  if (target.tagName === "LI") calculate(target.innerHTML, false);
 };
